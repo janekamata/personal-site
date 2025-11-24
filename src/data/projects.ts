86c766dd9dc2ef5068b6465bf9ace0ffb6f40f2d
Jane Kamata <janekamata8@gmail.com>
2025-11-24 01:13:58 -0500

    Putting image slash back

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,7 +12,7 @@ const data: Project[] = [
     title: 'Boston Climate Action Network',
     subtitle: 'Code4Community Client Project',
     link: 'https://github.com/Code-4-Community/bcan',
-    image: 'images/projects/bcan2.png',
+    image: '/images/projects/bcan2.png',
     date: '2025-12-04',
     desc:
       'Collaborate with a nonprofit client to deliver a tailored grant analytics dashboard for tracking grant win rate and costs using  Recharts with React, NestJS, AWS DynamoDB, and Jest. ' +
@@ -22,7 +22,7 @@ const data: Project[] = [
     title: 'NBA Globalization',
     subtitle: 'Cleartelligence Chart Champ Runner-Up 2024',
     link: 'https://public.tableau.com/app/profile/jane.kamata/viz/NBAGlobalizationChartChamps/NBAGlobalization',
-    image: 'images/projects/nba_globalization.png',
+    image: '/images/projects/nba_globalization.png',
     date: '2024-10-12',
     desc:
       'Analzed 455,663 basketball game records and 15,935 athelete data points from three datasets to creating runner-up infographic. ' +
@@ -33,7 +33,7 @@ const data: Project[] = [
     title: 'Code4Community Website Redesign',
     subtitle: 'Code4Community Core Infrastructure Project',
     link: 'https://www.c4cneu.com/',
-    image: 'images/projects/c4c.png',
+    image: '/images/projects/c4c.png',
     date: '2024-9-05',
     desc:
       'Contributed to a website redesign of the Code4Community website ' +
@@ -42,7 +42,7 @@ const data: Project[] = [
   {
     title: 'A Thousands Words are Worth a Picture',
     subtitle: 'Iron Viz Qualifiers 2024',
-    image: 'images/projects/thousand_words.png',
+    image: '/images/projects/thousand_words.png',
     date: '2023-10-27',
     desc:
       'Wrangled and analyzed 451,583 location data points and 345,719 words from 51,170 messages to create final visualization. ' +
